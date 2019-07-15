6bc2a42841842b8b14a3621d8ff1b117cfc70acb
Ihor <ShadoofJumper@gmail.com>
2019-07-15 21:18:13 +0300

    Create first logi for memory game
    Add sprites for game, add two objects on scene, card and back card surface. Also add script for changing back surface visible.

---
+++ 2DSimpleGame/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+
+    }
+}
