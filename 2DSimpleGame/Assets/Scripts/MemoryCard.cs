5c12ed344fcaaa567021ffb42b16215851f69d29
Ihor <ShadoofJumper@gmail.com>
2019-07-16 22:15:03 +0300

    Add logic creatinc card
    Add logic for programm creating card.

--- 2DSimpleGame/Assets/Scripts/MemoryCard.cs
+++ 2DSimpleGame/Assets/Scripts/MemoryCard.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class MemoryCard : MonoBehaviour
 {
+    [SerializeField] private SceneController controller;
     [SerializeField] private GameObject cardBack;
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
     public void OnMouseDown()
     {
         if (cardBack.activeSelf)
---
+++ 2DSimpleGame/Assets/Scripts/SceneController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public int gridRow = 2;
+    public int gridColumn = 4;
+    public float offsetX = 2.0f;
+    public float offsetY = 2.5f;
+
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+   
+
+    void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;
+
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
+
+        for (int x = 0; x < gridColumn; x++)
+        {
+            for (int y = 0; y < gridRow; y++)
+            {
+                MemoryCard card;
+                if (x == 0 && y == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+
+                int index = y * gridColumn + x;
+                int id = numbers[index];
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX * x) +  startPos.x;
+                float posY = -(offsetY * y) + startPos.y;
+
+
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+    }
+
+    private int[] ShuffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+
+        }
+
+        return newArray;
+    }
+}
